ea7be9effffd7f3df2332d33e1b06016191c3b85
Shubham Rindhe <shubhamrindhe00@gmail.com>
2020-01-01 23:30:38 +0530

    serpent.prototype.render()

--- scripts/serpent.js
+++ scripts/serpent.js
@@ -46,6 +46,10 @@ class Serpent{
 		
 	}
 	
+	
+	seek(x,y){
+	}
+	
 	charm(towards){
 		switch(towards){
 			case '^':
@@ -79,6 +83,109 @@ class Serpent{
 			default:
 			
 		}
+	}
+	
+	
+	render(ctx){
+		
+		
+		ctx.beginPath();
+		ctx.fillStyle = 'black';
+		ctx.arc( this.body[0].x*w + w*0.5, this.body[0].y*w + w*0.5 , w*0.5 , 0 , Math.PI*2 ,true );
+		ctx.fill();
+		ctx.closePath();
+		
+		
+		ctx.strokeStyle = "black";
+		ctx.beginPath();
+		ctx.moveTo(this.body[0].x * w + w*0.5 ,this.body[0].y * w + w*0.5 );
+		
+		for(var i=1;i<this.body.length;++i){
+								
+			var abs_x = Math.abs(this.body[i-1].x - this.body[i].x);
+			var abs_y = Math.abs(this.body[i-1].y - this.body[i].y);
+			
+			
+			
+			
+			
+			if(
+				(abs_x==1||abs_x==0)
+					&&
+				(abs_y==1||abs_y==0)
+			){
+				ctx.lineTo( this.body[i].x*w + w*0.5 , this.body[i].y*w + w*0.5 );
+				ctx.lineCap = "round";
+				ctx.lineJoin = "round";//"bevel|round|miter";
+				ctx.lineWidth = w - 2;
+			}else{
+				ctx.stroke();
+				ctx.closePath();	
+				ctx.beginPath();
+				ctx.moveTo( this.body[i].x*w + w*0.5 ,this.body[i].y * w + w*0.5 );
+				ctx.lineTo( this.body[i].x*w + w*0.5 ,this.body[i].y*w + w*0.5 );
+				ctx.lineCap = "round";
+				ctx.lineJoin = "round";//"bevel|round|miter";
+				ctx.lineWidth = w - 2;
+			}
+			//ctx.fillRect( boa.body[i].x*w , boa.body[i].y*w , w-2 , w-2 );
+			//if(trail[i].x==px && trail[i].y==py){tail=1;}
+		}
+		//ctx.fill();
+		ctx.stroke();
+		ctx.closePath();
+							
+		for(var i=1;i<this.body.length;++i){
+			ctx.beginPath();
+			ctx.fillStyle = 'yellow';
+			ctx.arc( this.body[i].x*w + w*0.5, this.body[i].y*w + w*0.5 , 1 , 0 , Math.PI*2 ,true );
+			ctx.fill();
+			ctx.closePath();
+		}
+
+		
+		ctx.save();					
+		ctx.translate(this.body[0].x*w + w*0.5, this.body[0].y*w + w*0.5 );
+		switch(this.dirChar){
+			case '^':
+				ctx.rotate(0);
+				break;
+			case '>':
+				ctx.rotate(Math.PI/2);
+				break;
+			case 'v':
+				ctx.rotate(Math.PI);
+				break;
+			case '<':
+				ctx.rotate(-Math.PI/2);
+				break;
+		}	
+							
+		ctx.beginPath();
+		ctx.moveTo(0,0);
+		ctx.lineTo(0,-10);
+		ctx.strokeStyle = 'red';
+		ctx.lineWidth = 3;
+		ctx.stroke();
+		ctx.closePath();
+							
+		/*				
+		ctx.beginPath();
+		ctx.arc(-3,-5,2,0,Math.PI*2,true);
+		ctx.fillStyle = '#fff';
+		ctx.fill();
+		ctx.closePath();
+							
+		ctx.beginPath();
+		ctx.arc( 3,-5,2,0,Math.PI*2,true);
+		ctx.fillStyle = '#fff';
+		ctx.fill();
+		ctx.closePath();
+		*/					
+		ctx.restore();
+		
+		
+		
 	}
 	
 } 
